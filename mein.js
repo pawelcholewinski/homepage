c0b0387bbd025d1e93248b6ba47415ae84d3339e
Paweł Cholewiński <wtf@Air-Pawel.lan>
2020-05-06 15:00:05 +0200

    initial commit

---
+++ mein.js
@@ -0,0 +1,6 @@
+const firstName = 'Paweł';
+const age = 36;
+console.log(firstName);
+console.log(age);   
+console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat`);
+alert(`Cześć nazywam się ${firstName}`)
